8d3982151e9323ec35471e918d034481df8fc5ce
Hussein Gabriel Rahman Nuñez <hrahman@coris.ec>
2020-03-10 17:11:42 -0500

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies',    component: MoviesComponent  },
+  { path: 'movie/:id', component: MovieComponent   },
+  { path: '',   pathMatch: 'full', redirectTo: '/movies' },
+  { path: '**', pathMatch: 'full', redirectTo: '/movies' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    NavbarComponent,
+    MoviesComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
