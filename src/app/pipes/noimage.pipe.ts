536ada12dfa7f5f421a96813465aa95a3052159c
jggan13 <joseganchozo@outlook.com>
2020-03-28 19:48:39 -0500

    Update noimage.pipe.ts

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -6,9 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NoimagePipe implements PipeTransform {
 
   //https://espamidiomas.azurewebsites.net
-  
+  //https://api.cmfconstructora.com.ec
   transform(value: string): any {
-    const url = "https://api.cmfconstructora.com.ec/images/";
+    const url = "https://espamidiomas.azurewebsites.net/images/";
     return `${url}${value}`;
   }
 
