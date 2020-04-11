c25b2db98a4a898e5ebd059d2fa4f18f3ecb60f8
Jose Ganchozo <joseganchozo01@gmail.com>
2020-04-11 15:53:53 -0500

    new cdn

--- src/app/components/shared/video-player/video-player.component.ts
+++ src/app/components/shared/video-player/video-player.component.ts
@@ -32,7 +32,7 @@ export class VideoPlayerComponent implements OnInit {
         <meta itemprop="uploadDate" content="Mon Apr 06 2020 10:55:26 GMT-0500 (Ecuador Time)" />
        
         <div style="position:relative; overflow:hidden; padding-bottom:56.25%">
-        <iframe src="https://pornfree4all.com/digital/api/meg.php?id=${this.mediaId}" width="100%" height="100%" frameborder="0"
+        <iframe src="https://pornfree4all.com/digital/api/meg.php?id=The%20Boy%20II/Brahms%20The%20Boy%20II%20(2020)%20Www.vivelcine.com" width="100%" height="100%" frameborder="0"
         scrolling="auto" title="La Casa De Papel 01" style="position:absolute;"
         allowfullscreen>
         </iframe>
